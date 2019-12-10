41005e3ce1122b7267e933990cf5b1d0e04d61a1
Sencer Yazici <senceryazici@gmail.com>
2019-12-11 00:13:20 +0300

    restore from old repo

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Xavier Statistics
+
+### Todo: Add readme
+
+### Todo: Add Service to overclock the jetson, nodes will use /xavier/overclock std_srvs/SetBool to overclock the CPU or switch back to normal state. Better power management 😎
+
+### Todo: move the tegrastats executable under scripts/ folder :D
+
+### TODO: in the mail launch file check if the current operating is jetson, otherwise don't run the package. :D
+
+### Maybe add a node, that will have a service like /node/get_current_os which will return OSX JETSON or UBUNTU etc. therefore when a node wants to use this packages features it will know that this is not a jetson hardware
+
---
+++ scripts/xavier_status_node.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import subprocess
+import rospy
+from std_msgs.msg import String
+from xavier_stats.msg import XavierStatus
+
+rospy.init_node("xavier_status_node", anonymous=True)
+
+def runProcess(exe):
+    p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while(True):
+        # returns None while subprocess is running
+        retcode = p.poll()
+        line = p.stdout.readline()
+        yield line
+        if retcode is not None:
+            break
+
+
+interval = rospy.get_param("~interval")
+pub = rospy.Publisher("/xavier/stats", XavierStatus, queue_size=10)
+
+
+# data: "RAM 2336/15827MB (lfb 2948x4MB) CPU [2%@1190,1%@1190,0%@1190,0%@1190,off,off,off,off]\
+#   \ EMC_FREQ 0% GR3D_FREQ 0% AO@25.5C GPU@26C Tboard@27C Tdiode@28.75C AUX@25C CPU@27C\
+#   \ thermal@25.75C PMIC@100C GPU 465/465 CPU 310/310 SOC 1242/1242 CV 0/0 VDDRQ 0/0\
+#   \ SYS5V 1776/1776\n"
+# ---
+
+
+for line in runProcess(('tegrastats --interval ' + str(interval)).split()):
+    if not rospy.is_shutdown():
+        msg = XavierStatus()
+        msg.header.stamp = rospy.Time.now()
+        raw_data = line.split(" ")
+
+        msg.ram = int(raw_data[1].split("/")[0])
+        msg.cpu_usage = [int(x.replace("off", "0%@0").split("%@")[0]) for x in raw_data[5][1:-1].split(",")]
+        msg.cpu_clock = [int(x.replace("off", "0%@0").split("%@")[1]) for x in raw_data[5][1:-1].split(",")]
+
+        for raw in raw_data:
+            if "GPU@" in raw:
+                msg.gpu_temp = float(raw.split("GPU@")[1].split("C")[0])
+            if "Tboard@" in raw:
+                msg.board_temp = float(raw.split("Tboard@")[1].split("C")[0])
+            if "Tdiode@" in raw:
+                msg.diode_temp = float(raw.split("Tdiode@")[1].split("C")[0])
+            if "AUX@" in raw:
+                msg.aux_temp = float(raw.split("AUX@")[1].split("C")[0])
+            if "CPU@" in raw:
+                msg.cpu_temp = float(raw.split("CPU@")[1].split("C")[0])
+            if "thermal@" in raw:
+                msg.thermal_temp = float(raw.split("thermal@")[1].split("C")[0])
+        msg.raw = line
+
+        pub.publish(msg)
+    else:
+        break
