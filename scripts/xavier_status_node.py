f2a201eeff5913313bbd116b4ae070fd609f232c
Sencer Yazici <senceryazici@gmail.com>
2020-01-12 08:45:05 +0000

    Add executable

--- scripts/xavier_status_node.py
+++ scripts/xavier_status_node.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import subprocess
 import rospy
-from std_msgs.msg import String, Float32
+from std_msgs.msg import String, Float32, Int16
 from xavier_stats.msg import XavierStatus
+import rospkg
 
 rospy.init_node("xavier_status_node", anonymous=True)
 
@@ -23,23 +24,24 @@ pub_gputemp = rospy.Publisher("/turquoise/xavier/gpu_temp", Float32, queue_size=
 pub_cputemp = rospy.Publisher("/turquoise/xavier/cpu_temp", Float32, queue_size=10)
 pub_boardtemp = rospy.Publisher("/turquoise/xavier/board_temp", Float32, queue_size=10)
 pub_auxtemp = rospy.Publisher("/turquoise/xavier/aux_temp", Float32, queue_size=10)
-
-# pub_ram = rospy.Publisher("/turquoise/xavier/ram", Float32, queue_size=10)
-
+pub_ram = rospy.Publisher("/turquoise/xavier/ram", Float32, queue_size=10)
+pub_cores = rospy.Publisher("/turquoise/xavier/cores_alive", Int16, queue_size=10)
 # data: "RAM 2336/15827MB (lfb 2948x4MB) CPU [2%@1190,1%@1190,0%@1190,0%@1190,off,off,off,off]\
 #   \ EMC_FREQ 0% GR3D_FREQ 0% AO@25.5C GPU@26C Tboard@27C Tdiode@28.75C AUX@25C CPU@27C\
 #   \ thermal@25.75C PMIC@100C GPU 465/465 CPU 310/310 SOC 1242/1242 CV 0/0 VDDRQ 0/0\
 #   \ SYS5V 1776/1776\n"
 # ---
 
-
-for line in runProcess(('tegrastats --interval ' + str(interval)).split()):
+rospack = rospkg.RosPack()
+pkg_path = rospack.get_path("xavier_stats")
+for line in runProcess((pkg_path + '/scripts/tegrastats --interval ' + str(interval)).split()):
     if not rospy.is_shutdown():
         msg = XavierStatus()
         msg.header.stamp = rospy.Time.now()
         raw_data = line.split(" ")
 
         msg.ram = int(raw_data[1].split("/")[0])
+        cores_alive = len(raw_data[5].split("off")) - 1
         msg.cpu_usage = [int(x.replace("off", "0%@0").split("%@")[0]) for x in raw_data[5][1:-1].split(",")]
         msg.cpu_clock = [int(x.replace("off", "0%@0").split("%@")[1]) for x in raw_data[5][1:-1].split(",")]
 
@@ -64,5 +66,8 @@ for line in runProcess(('tegrastats --interval ' + str(interval)).split()):
         pub_cputemp.publish(Float32(msg.cpu_temp))
         pub_boardtemp.publish(Float32(msg.board_temp))
         pub_auxtemp.publish(Float32(msg.aux_temp))
+        pub_ram.publish(Float32(msg.ram))
+        pub_cores.publish(Int16(cores_alive))
     else:
         break
+
